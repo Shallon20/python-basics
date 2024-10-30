988caa37cad5413283605f18939392ce8224c33c
Shallon <shallonmaria2001@gmail.com>
2024-10-30 08:21:16 +0200

    loops

---
+++ lesson9.py
@@ -0,0 +1,11 @@
+# loops
+while True:
+    print("Hello, loops")
+    break # stop loop from running
+
+scores = [90,76, 56, 81, 78, 99, 54, 78, 34, 65, 78,45, 23, 56, 89, 89, 45, 68, 45, 77, 76]
+
+# for each loop
+for score in scores:
+    if score >= 50:
+        print(score)
