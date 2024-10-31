cd05be6e4de7ad7769cf91c26dd5f73e564b91b3
Shallon <shallonmaria2001@gmail.com>
2024-10-31 07:38:52 +0200

    Created a python file for simple basic bank functions

---
+++ lesson12.py
@@ -0,0 +1,6 @@
+class Account:
+    def __init__(self, full_name, acc_num, phone, balance):
+        self.full_name = full_name
+        self.acc_num = acc_num
+        self.phone = phone
+        self.balance = balance
